df0889b7706ecf5e266436cd96054a2d524c53cc
Mr-Martins29 <duollinni29@gmail.com>
2025-07-12 23:41:15 +0000

    Correção da carta 1

---
+++ Carta_1SuperTrunfo.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+
+    // Carta 1
+char carta1 = '1';
+char estado1 = 'T';
+char codigo1[5] = "T01";
+char cidade1[15] = "Barrolândia";
+int pessoas1 = 9876;
+int local1 = 5;
+float espaco1 = 10.123;
+float numero1 = 14.7;
+
+printf("Número da Carta: %c \n", carta1);
+
+printf("Estado: %c \n", estado1);
+
+printf("Código: %s \n", codigo1);
+
+printf("Cidade: %s \n", cidade1);
+
+printf("Habitantes: %d \n", pessoas1);
+
+printf("Tamanho da área: %.3f \n", espaco1);
+
+printf("PIB: %.1f \n", numero1);
+
+// Imprime carta 1
+
+scanf("%c", &carta1);
+
+scanf(" %c", &estado1);
+
+scanf("%s", codigo1);
+
+scanf("%s", cidade1);
+
+scanf("%d", &pessoas1);
+
+scanf("%d", &local1);
+
+scanf("%f", &espaco1);  
+
+scanf("%f", &numero1);
+
+return 0;
+
+
+}

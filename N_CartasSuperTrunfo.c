457d3838b0a1a74ede4410eb447ff360e7c970ab
Mr-Martins29 <duollinni29@gmail.com>
2025-07-12 19:09:55 +0000

    Cadastro e impressão da carta 1

--- N_CartasSuperTrunfo.c
+++ N_CartasSuperTrunfo.c
@@ -7,10 +7,20 @@
 // Definição, carta 1
 
 int main(){
-char estado1;
-char codigo1[20], cidade1[20];
-int populacao1, turismo1;
-float area1, pib1;
+
+// Carta 1
+char carta1 = '1';
+char estado1 = 'T';
+char codigo1[20] = "T01";
+char cidade1[20] = "Palmas";
+int populacao1 = 302.692;  
+int turismo1 = 58;
+float area1 =  2.219;
+float pib1 = 58.1; 
+
+// Imprime 1
+printf("Carta: %s\n", carta1);
+scanf("%s", carta1);
 
 printf("Estado: %s\n", estado1);
 scanf("%s", estado1);
@@ -33,11 +43,6 @@ scanf("%f", &pib1);
 printf("Pontos Turísticos: %d\n", turismo1);
 scanf("%d", &turismo1);
 
-printf("Estado é: %s\n - Código é: %s\n - Cidade é: %s\n", estado1, cidade1, codigo1);
-
-printf("População é: %d\n - Pontos Turísticos são: %d\n", populacao1, turismo1);
-
-printf("A área é: %f\n - O PIB é: %f", area1, pib1);
 
 return 0;
 
